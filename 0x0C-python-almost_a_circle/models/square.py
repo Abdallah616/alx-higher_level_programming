35ffe0521faa5232231726f869634a4901709205
Abdallah616 <yassinabdou59@gmail.com>
2023-12-11 09:52:43 -0800

    task solved

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Module for Square class.'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Square class.'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor.'''
+        super().__init__(id, size, size, x, y)
+        def __str__(self):
+        '''Returns string info about this square.'''
+            return '[{}] ({}) {}/{} - {}'.\
+                format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+        @property
+        def size(self):
+            '''size of this square.'''
+            return size
+
+        @size.setter
+        def size(self, value):
+            self.height = value
+            self.width = value
