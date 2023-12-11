1933ae7fe47ca076bdd04b46f7e3facdecfc8a1d
Abdallah616 <yassinabdou59@gmail.com>
2023-12-11 09:54:55 -0800

    task solved

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,17 +9,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor.'''
         super().__init__(id, size, size, x, y)
-        def __str__(self):
+
+    def __str__(self):
         '''Returns string info about this square.'''
-            return '[{}] ({}) {}/{} - {}'.\
-                format(type(self).__name__, self.id, self.x, self.y, self.width)
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
-        @property
-        def size(self):
-            '''size of this square.'''
-            return size
+    @property
+    def size(self):
+        '''size of this square.'''
+        return size
 
-        @size.setter
-        def size(self, value):
-            self.height = value
-            self.width = value
+    @size.setter
+    def size(self, value):
+        self.height = value
+        self.width = value
