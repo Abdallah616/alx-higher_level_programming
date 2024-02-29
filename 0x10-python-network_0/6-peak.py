13dbb0b8b1ee558082e131ca4fc67e00cd5d1bd4
Abdallah616 <yassinabdou59@gmail.com>
2024-02-29 12:38:10 -0800

    task solved

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Module that finds a peak in a list of integers"""
-
+"""
+This function finds a peak in a list of unsorted integers.
+A peak is an element greater than or equal to its neighbors.
+"""
 
 def find_peak(list_of_integers):
-    list_ = list_of_integers
-    # if there is no list of integers return None
-    if list_ == []:
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
         return None
-    length = len(list_)
 
-    start, end = 0, length - 1
-    while start < end:
-        mid = start + (end - start) // 2
-        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
-            return list_[mid]
-        if list_[mid - 1] > list_[mid + 1]:
-            end = mid
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
         else:
-            start = mid + 1
-    return list_[start]
+            low = mid + 1
+
+    return list_of_integers[low] if low == 0 or list_of_integers[low - 1] <= list_of_integers[low] else None
